1f65e8f5b3873045b52bbcd97c64923832986546
Muhammad Zulqarnain <mzh175667@gmail.com>
2022-10-17 02:05:39 -0700

    done

--- validators/checkOutSchema.js
+++ validators/checkOutSchema.js
@@ -3,6 +3,7 @@ const Joi = require("joi");
 const checkOutSchema = Joi.object({
   checkOutTime: Joi.string().required(),
   totalTime: Joi.string().required(),
+  reason: Joi.string(),
 });
 
 module.exports = checkOutSchema;
